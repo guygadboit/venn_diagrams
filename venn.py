50fffb33d995740a47d02f63dde8cfca8fe920e4
Guy Gadboit <guy@invalid.org>
2024-07-16 11:19:25 +0100

    This seems to work.
    Not so hard to find those simplexes. You just have to solve for the
    right edge lengths.
    
    Interesting you need an edge length of sqrt(2) it seems, found by trial
    and error basically, for the Venn diagrams to all work.

--- venn.py
+++ venn.py
@@ -1,49 +1,100 @@
 import numpy as np
-import scipy.linalg as la
+import itertools
 from numpy.linalg import norm
 from pdb import set_trace as brk
+
+
+def solve_quadratic(a, b, c):
+	"""Solve a^2x + bx + c = 0"""
+	t = np.sqrt(b*b - 4*a*c)
+	return (-b + t) / (2*a), (-b - t) / (2*a)
 
 
 def find_centre(vertices):
 	return sum(vertices)/len(vertices)
 
 
-def recenter(vertices):
-	"""Recentre and normalize the vertices"""
+def recentre(vertices):
 	centre = find_centre(vertices)
 	for i, _ in enumerate(vertices):
 		vertices[i] -= centre
-		vertices[i] /= norm(vertices[i])
 
 
 class Simplex:
-	def __init__(self, dimensions):
-		self.vertices = self._find_vertices(dimensions, 1.9)
+	def __init__(self, num_vertices, edge_len):
+		self.vertices = self._find_vertices(num_vertices, edge_len)
 
-	def _find_vertices(self, dimensions, edge_len):
-		n = dimensions
+	def _find_vertices(self, n, edge_len):
+		"""Construct a nice equilateral simplex centred on the origin, with
+		n-1 dimensions"""
 		assert n >= 1
 
-		# Our 0D simplex can just be a point at the origin
-		vertices = [np.zeros((n, 1))]
+		# Start with a 0D simplex, which is just a point at the origin
+		vertices = [np.zeros(n)]
 
-		# To add another dimension start with the centre of the points you have
-		# and extend it "upwards" into the new dimension. Keep the whole thing
-		# equilateral as you go by re-centering and re-normalizing
+		# To add another dimension start at the centre of the points we have
+		# and extend "upwards" into the new dimension, keeping things centered
+		# and equilateral as we go.
 		for i in range(n):
-			new_vertex = np.zeros((n, 1))
-			new_vertex[i] = 1.0
+			# The centre is already at the origin, since we recentre at every
+			# step
+			new_vertex = np.zeros(n)
+
+			# If this is the first edge, it's easy to make it right the length
+			if len(vertices) == 1:
+				new_vertex[i] = edge_len
+			else:
+				# Otherwise find the right length by solving the quadratic
+				# equation to make the edge length to the previous vertex
+				# correct. Because it's all symmetrical the others should all
+				# be the right lengths too.
+				new_vertex[i] = 1
+				b = -2 * np.dot(new_vertex, vertices[-1])
+				c = np.dot(vertices[-1], vertices[-1]) - edge_len**2
+				new_vertex[i] = solve_quadratic(1, b, c)[0]
+
 			vertices.append(new_vertex)
-			recenter(vertices)
+			recentre(vertices)
 
 		return vertices
 
+	def print(self):
+		for i, v in enumerate(self.vertices):
+			j = (i+1) % len(self.vertices)
+			print(v, norm(self.vertices[j]-v))
+
+	def find_shared_point(self, indices):
+		"""Our "Venn diagram" consists of a unit sphere at each vertex of the
+		simplex. Indices identifies a list of vertices and therefore spheres.
+		So, e.g. [0,1] means the spheres at vertices 0 and 1. Find a point that
+		is inside the specified spheres and not inside any others, and verify
+		it"""
+		ret = find_centre([self.vertices[i] for i in indices])
+
+		# Which spheres is the point inside?
+		inside = []
+		for i, v in enumerate(self.vertices):
+			if norm(ret - v) < 1.0:
+				inside.append(i)
+
+		if inside != sorted(indices):
+			brk()
+
+		print("{} is inside spheres {}/{} and only inside those ones".format(
+			ret, inside, len(self.vertices)))
+		return ret
+
+
+def test(num_vertices, edge_len):
+	s = Simplex(num_vertices, edge_len)
+	for length in range(1, num_vertices):
+		for c in itertools.combinations(range(num_vertices), length):
+			s.find_shared_point(c)
+
 
 def main():
-	s = Simplex(4)
-	for i, v in enumerate(s.vertices):
-		j = (i+1) % len(s.vertices)
-		print(v.transpose(), norm(v), norm(s.vertices[j]-v))
+	for num_vertices in range(3, 100):
+		test(num_vertices, np.sqrt(2))
 
 
 if __name__ == "__main__":
